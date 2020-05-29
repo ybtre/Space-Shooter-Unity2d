4da04328094bd6df8dd5d816e0863eac1f8a4582
Hristo Vuchev <megalodon7892@gmail.com>
2020-05-29 21:49:34 +0000

    simple fire bullet logic and class

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
+public class Bullet : MonoBehaviour {
+
+    [SerializeField] float speed = 5f;
+    [SerializeField] bool hasFired = false;
+    [SerializeField] bool isAlive = false;
+    Player player;
+    Rigidbody2D myRigidBody2D;
+    Vector2 bulletSpawnOnGunVector;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        // bulletSpawnOnGunVector = transform.position - player.transform.position;
+        player = FindObjectOfType<Player>();
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+
+        // bulletSpawnOnGunVector.x = player.transform.position.x + 2;
+        // spawnBulletAtGunPosition();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // spawnBulletAtGunPosition();
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            transform.position = player.transform.position;
+            myRigidBody2D.velocity = new Vector2(-speed, 0);
+        }
     }
+
+    // private void spawnBulletAtGunPosition() {
+    //     Vector2 playerShipPos = new Vector2(player.transform.position.x, player.transform.position.y);
+    //     transform.position = playerShipPos + bulletSpawnOnGunVector;
+    // }
 }
